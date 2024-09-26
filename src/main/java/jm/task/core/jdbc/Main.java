4c05fecb5f5303e37642a1567ac188da765a99ab
Yaderkal <amesheryakov22813337@mail.ru>
2024-09-26 14:46:51 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        Util.getConnection();
+        UserService userDao = new UserServiceImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Ivan", "Ivanov", (byte) 20);
+        userDao.saveUser("Cheslav", "Rochev", (byte) 25);
+        userDao.saveUser("Nick", "Valentine", (byte) 31);
+        userDao.saveUser("Rick", "Grimes", (byte) 38);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,43 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl extends Util implements UserService  {
+    UserDao userDao = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-
+        this.userDao.createUsersTable();
+        System.out.println("Таблица создана");
     }
 
-    public void dropUsersTable() {
-
+    public void dropUsersTable() throws SQLException {
+        this.userDao.dropUsersTable();
+        System.out.println("удалена таблица");
     }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        this.userDao.saveUser(name, lastName, age);
+        System.out.println("сохранен пользователь " + name + " " + lastName + " " + age);
     }
 
     public void removeUserById(long id) {
-
+        this.userDao.removeUserById(id);
+        System.out.println("пользователь с id " + id + " удален");
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+        System.out.println(this.userDao.getAllUsers());
+        return this.userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        this.userDao.cleanUsersTable();
+        System.out.println("Таблица очищена");
     }
 }
