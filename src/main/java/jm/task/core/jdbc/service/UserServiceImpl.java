f2e73c632ff0176b53ae35984428d56743178e52
Yaderkal <amesheryakov22813337@mail.ru>
2026-05-17 16:24:09 +0300

    1

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,11 +2,9 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
